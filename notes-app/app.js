ecc2c68724ca5756b527f0e4e3d0c30715b06292
Ryoung27 <RRYoung89@gmail.com>
2019-07-07 16:06:54 -0500

    chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,15 @@
+const chalk = require('chalk');
 const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const message = getNotes()
 
 
-console.log(message)
+console.log(chalk.blue(message))
+
+console.log(chalk.green.bold.inverse("Success!"))
+
+console.log(chalk.dim.blue("test"))
 
 console.log(validator.isEmail('blank@blank.com'))
 console.log(validator.isURL('rryoung.com'))
