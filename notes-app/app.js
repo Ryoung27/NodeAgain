21d459830fc1d97b49bf83a7435826666888e2c1
Ronnie Young <ronnieyoung@Ronnies-MacBook-Pro.local>
2019-07-08 14:38:41 -0500

    adding notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,83 @@
 const chalk = require('chalk');
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
+const yargs = require('yargs')
 
-const command = process.argv[2]
+// const command = process.argv[2]
+//add, remove, read, list
 
-console.log(process.argv)
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    builder: {
+        title: {
+            describe: 'Note title.',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'A body.',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        notes.addNote(argv.title, argv.body)
+    }
+})
 
-if (command === 'add'){
-    console.log("Adding note.")
-} else if (command === 'remove'){
-    console.log("Removing note.")
-}
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note.',
+    builder:{
+        title: {
+            describe: 'Remove a note.',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Removing the note: ' +argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Print the list.',
+    builder:{
+        title:{
+            describe: 'Print the list.',
+            demandOption: false,
+            type: 'string'
+        }
+    },
+    handler: function(){
+        console.log('Show the list.')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read the list.',
+    builder:{
+        title:{
+            describe: 'Read the list.',
+            demandOption: false,
+            type: 'string'
+        }
+    },
+    handler: function(){
+        console.log('Print the list.')
+    }
+})
+
+
+// console.log(process.argv)
+// console.log(yargs.argv)
+
+yargs.parse()
+
+// if (command === 'add'){
+//     console.log("Adding note.")
+// } else if (command === 'remove'){
+//     console.log("Removing note.")
+// }
