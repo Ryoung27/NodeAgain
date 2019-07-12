c6281f5ae7a753c051c48ee111a6d6e2be4925e3
Ronnie Young <ronnieyoung@Ronnies-MacBook-Pro.local>
2019-07-12 13:48:24 -0500

    working with units

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url= 'https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/37.8267,-122.4233'
+const url= 'https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/36.0331,86.7828?units=us'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.currently)
+    console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
 })
