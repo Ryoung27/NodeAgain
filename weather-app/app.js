46325fedad082dbc2988b01adfd87229f42e474f
Ronnie Young <ronnieyoung@Ronnies-MacBook-Pro.local>
2019-07-12 13:06:29 -0500

    currently

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
+const url= 'https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/37.8267,-122.4233'
 
-setTimeout(() =>{
-    console.log('2 Second Timer')
-}, 2000)
-
-setTimeout(() =>{
-    console.log('0 Second Timer')
-}, 0)
-
-
-console.log('Stopping')
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
