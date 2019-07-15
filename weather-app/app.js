3b32da771e89c221162aefdbd446083b43040903
Ronnie Young <ronnieyoung@Ronnies-MacBook-Pro.local>
2019-07-15 10:46:36 -0500

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,13 +3,14 @@ const request = require('request')
 const url= 'https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/36.0331,86.7828?units=us'
 
 request({url: url, json: true}, (error, response) => {
-    console.log(response.body.currently)
-    console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
+    if (error){
+        console.log("Unable to connect to weather services.")
+    }else {
+        console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is a " + response.body.currently.precipProbability + "% chance of rain.")
+    }
+
 })
 
-//Geocoding
-//Address -> Lat/Long -> Weather
-// pk.eyJ1IjoicnlvdW5nMjciLCJhIjoiY2p5NGlvZXhvMDB2MjNtbzBwOXE2cTNpMiJ9.RVtsgLTT9EhIDPIFT0wVlg
 
 const geocodeURL= 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicnlvdW5nMjciLCJhIjoiY2p5NGlvZXhvMDB2MjNtbzBwOXE2cTNpMiJ9.RVtsgLTT9EhIDPIFT0wVlg&limit=1'
 
