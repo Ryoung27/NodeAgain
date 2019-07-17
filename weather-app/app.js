945f19f87c4e8146bc8e37e9adbfe833d0329405
Ronnie Young <ronnieyoung@Ronnies-MacBook-Pro.local>
2019-07-17 08:46:38 -0500

    added forecast section

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const geocode = require('./geocode/geocode.js')
+const forecast = require('./utils/forecast')
 
 // const url= 'https://api.darksky.net/forecast/8691e730fc26412960eaba6814fdedbe/36.0331,86.7828?units=us'
 
@@ -14,24 +15,12 @@ const geocode = require('./geocode/geocode.js')
 
 // })
 
-
-// const geocodeURL= 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicnlvdW5nMjciLCJhIjoiY2p5NGlvZXhvMDB2MjNtbzBwOXE2cTNpMiJ9.RVtsgLTT9EhIDPIFT0wVlg&limit=1'
-
-// request({url: geocodeURL, json: true}, (error, response) => {
-//     if (error){
-//         console.log("Unable to find location.")
-//     } else if(response.body.features.length === 0){
-//         console.log('Unable to connect to find location.')
-//     }else{
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(latitude, longitude)
-//     }   
-// })
-
-
-
-geocode('Nashville', (error, data) =>{
+forecast(-75.7008, 44.1545, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
+
+// geocode('Nashville', (error, data) =>{
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
