13e9d53a6bbce9d8b325573f8489f9398687e242
Ronnie Young <ronnie.young@plainode.io>
2019-07-29 09:27:48 -0500

    delete

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,62 +1,30 @@
-//CRUD Operations create, read, update, delete
+// CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if(error){
-        return console.log('Unable to connect to database.')
+    if (error) {
+        return console.log('Unable to connect to database!')
     }
 
-    const db = client.db(databaseName);
+    const db = client.db(databaseName)
+    
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
 
-//     // db.collection('users').insertOne({
-//     //     name: 'Richie',
-//     //     age: 29
-//     // }, (error, result) => {
-//     //     if (error){
-//     //         return console.log("Unable to insert user.")
-//     //     }
-
-//     //     console.log(result.ops)
-//     // })
-
-//     db.collection('users').insertMany([{
-//         name: 'Ronnie',
-//         age: 29
-//     }, {
-//         name: 'Random',
-//         age: 400
-//     }
-// ], (error, result) => {
-//     if (error){
-//         return console.log('Unable to insert documents.')
-//     }
-
-//     console.log(result.ops)
-// })
-
-
-    db.collection('tasks').insertMany([
-        {
-        description: 'A string for descriptions',
-        completed: true
-        },
-        {
-        description: 'Clean the House',
-        completed: false
-        },{
-        description: 'run',
-        completed: false
-        }
-
-], (error, result) => {
-    if(error){
-        return console.log('Unable to insert tasks.')
-    }
-    console.log(result.ops)
+    db.collection('tasks').deleteOne({
+        description: "Clean the house"
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 })
