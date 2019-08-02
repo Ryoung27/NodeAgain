49aa5d8510345e89d4a0dae26b8999f1458ced14
Ronnie Young <ronnie.young@plainode.io>
2019-08-02 11:00:11 -0500

    almost at api

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,43 +1,80 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewURLParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         lowercase: true,
+//         validate(value){
+//             if(value.toLowerCase().includes('password')){
+//                 throw new Error('Password cannot contain "password".')
+//             }
+//         }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Email is invalid.')
+//             }
+//         }
+//     }, 
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value){
+//             if(value < 0){
+//                 throw new Error('Age must be a positive number.')
+//             }
+//         }
+//     }
+// })
 
 const Task = mongoose.model('Tasks', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
+
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 
 })
 
-const run = new Task({
-    description: 'Go for a run.',
-    completed: true
+const task = new Task({
+    description: 'Learn Mongoose.',
+    completed: false
 })
 
-run.save().then(() =>{
-    console.log(run)
+task.save().then(() =>{
+    console.log(task)
 }).catch((error) => {
     console.log('Error', error)
 })
 
 // const me = new User({
-//     name: 'Richie',
-//     age: 29
+//     name: 'Mike',
+//     email: '   Mike@mike.com',
+//     password: 'RandomPS'
 // })
 
 // me.save().then(() => {
